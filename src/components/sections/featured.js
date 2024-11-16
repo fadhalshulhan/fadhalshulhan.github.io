8d0e42648a415b79a36c2ee62baa56134ef9a104
Fadhal Shulhan <fadhal.shulhan@gmail.com>
2024-11-16 12:57:10 +0700

    Update featured.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -383,7 +383,7 @@ const Featured = () => {
                     <div className="project-links">
                       {cta && (
                         <a href={cta} aria-label="Course Link" className="cta">
-                          Learn More
+                          See Details
                         </a>
                       )}
                       {github && (
